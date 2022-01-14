f16e27e5a3c7bb80ce6b9b67d3bd0c799e02d055
Bui The Vinh <vinhbuith32412000@gmail.com>
2022-01-14 10:03:10 +0700

    Project Completed

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+const blackMode = document.querySelector("#black-mode");
+const rainbowMode = document.querySelector("#rainbow-mode");
+const eraser = document.querySelector("#eraser");
+const clear = document.querySelector("#clear");
+
 const slider = document.querySelector("#slider");
 const slideOutput = document.querySelector("#slide-output");
 const grid = document.querySelector("#grid");
 const changeRatio = document.querySelector("#change-ratio");
-const clear = document.querySelector("#clear");
 
 slideOutput.textContent = `Pixel Ratio: ${slider.value} x ${slider.value}`
 
@@ -21,16 +25,61 @@ function createGrid(pixelRatio) {
         pixel.style.width = `${100 / pixelRatio}%`;
         pixel.style.height = `${100/ pixelRatio}%`;
         pixel.addEventListener("mousemove", (e) => {
-            e.target.classList.add("pixel-draw");
+            e.target.classList.add("pixel-draw-black");
         })
         grid.appendChild(pixel);
     }
 }
+
+function clearGrid() {
+    const pixels = document.querySelectorAll(".pixel");
+    pixels.forEach(pixel => {
+        pixel.className = "pixel";
+        pixel.style.backgroundColor = "";
+    })
+}
+
+function eraseGrid() {
+    pixels = document.querySelectorAll(".pixel");
+    pixels.forEach(pixel => {
+        pixel.addEventListener("mousemove", (e) => {
+            e.target.className = "pixel"; 
+            e.target.style.backgroundColor = "";
+            e.target.classList.add("pixel-eraser")
+        })
+    })
+}
+
+function toBlack() {
+    pixels = document.querySelectorAll(".pixel");
+    pixels.forEach(pixel => {
+        pixel.addEventListener("mousemove", (e) => {
+            e.target.className = "pixel";
+            e.target.style.backgroundColor = "";
+            e.target.classList.add("pixel-draw-black");
+        })
+    })
+}
+
+function toRainbow() {
+    pixels = document.querySelectorAll(".pixel");
+    pixels.forEach(pixel => {
+        pixel.addEventListener("mousemove", (e) => {
+            let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+            e.target.style.backgroundColor = randomColor;
+        })
+    })
+}
+
 
 createGrid(slider.value);
 changeRatio.addEventListener("click", () => {
     createGrid(slider.value);
 })
 
+clear.addEventListener("click", clearGrid);
+eraser.addEventListener("click", eraseGrid);
+blackMode.addEventListener("click", toBlack);
+rainbowMode.addEventListener("click", toRainbow);
 
 
